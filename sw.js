4bd7536a675abfcd4e630b92de92a9787acae9fd
ladlahamza <162014078+ladlahamza@users.noreply.github.com>
2024-08-18 20:08:40 +0500

    initial files

---
+++ sw.js
@@ -0,0 +1,26 @@
+const cacheName = 'pharmacy-app-cache';
+const assets = [
+  '/',
+  '/main.html',
+  '/manifest.json',
+  '/styles.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
